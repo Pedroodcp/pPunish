d7664912bbfaf6fd3b02687f7be0f08ff319dfae
Pedrodcp <joadelgg07@gmail.com>
2021-08-18 22:58:19 -0300

    18/08/2021

--- src/main/java/br/com/pedrodcp/ppunish/commands/checkpunish.java
+++ src/main/java/br/com/pedrodcp/ppunish/commands/checkpunish.java
@@ -2,12 +2,17 @@ package br.com.pedrodcp.ppunish.commands;
 
 import br.com.pedrodcp.ppunish.api.API;
 import br.com.pedrodcp.ppunish.api.PunishmentsAPI;
+import br.com.pedrodcp.ppunish.gui.PlayerPunishmentInfo;
 import br.com.pedrodcp.ppunish.gui.Punishments;
+import br.com.pedrodcp.ppunish.models.PunishmentAccount;
 import br.com.pedrodcp.ppunish.utils.FancyMessage;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import static br.com.pedrodcp.ppunish.gui.Punishments.*;
 
 public class checkpunish implements CommandExecutor {
 
@@ -15,6 +20,9 @@ public class checkpunish implements CommandExecutor {
 
     public static String playerName;
     public static String autorName;
+
+    protected String keyID;
+    protected Double keyDoubleID;
 
     @Override
     public boolean onCommand(CommandSender s, Command c, String label, String[] args) {
@@ -26,20 +34,43 @@ public class checkpunish implements CommandExecutor {
                 p.sendMessage("§cVocê não possui permissão para utilizar este comando.");
             } else {
                 if (args.length >= 1) {
-                    if (API.getAccount(args[0]) == null) {
-                        p.sendMessage("§cEste jogador não existe.");
+                    if (args[0].contains("#")) {
+                        keyID = args[0].replace("#", "");
                     } else {
-                        if (PunishmentsAPI.getAccount(args[0]) == null) {
-                            p.sendMessage("§cEste jogador não possui um histórico de punições.");
+                        keyID = args[0];
+                    }
+                    try {
+                        Double.parseDouble(keyID);
+                        keyDoubleID = Double.parseDouble(keyID);
+                    } catch (Exception e) {
+                        if (args[0].startsWith("#")) {
+                            new FancyMessage("§cUtilize ").text("§c/checkpunish §c<#ID>").suggest("/checkpunish #").text("§c.").send(p);
                         } else {
-                            playerName = args[0];
-                            autorName = p.getName();
-                            gui.gameGUI(p);
+                            if (API.getAccount(args[0]) == null) {
+                                p.sendMessage("§cEste jogador não existe.");
+                            } else {
+                                if (PunishmentsAPI.getAccount(args[0]) == null) {
+                                    p.sendMessage("§cEste jogador não possui um histórico de punições.");
+                                } else {
+                                    playerName = args[0];
+                                    autorName = p.getName();
+                                    gui.gameGUI(p);
+                                }
+                            }
+                            return false;
+                        }
+                    }
+                    for (PunishmentAccount punishmentAccounts : PunishmentAccount.accountsPunicoes) {
+                        if (punishmentAccounts.getID() == keyDoubleID.intValue()) {
+                            p.playSound(p.getLocation(), Sound.ORB_PICKUP, 5F, 1.0F);
+                            punishId = punishmentAccounts.getID();
+                            playerName = punishmentAccounts.getPlayerName();
+                            p.openInventory(new PlayerPunishmentInfo().getInventory());
                         }
                     }
                 } else {
                     p.sendMessage("");
-                    new FancyMessage("§c§l[PUNIÇÃO §c§l- §c§lERRO] §c§l➜ §fUtilize ").text("§b§n/checkpunish §b§n<jogador>").hover("§7Clique §7para §7copiar §7o §7comando").suggest("/checkpunish ").text("§f.").send(p);
+                    new FancyMessage("§c§l[PUNIÇÃO §c§l- §c§lERRO] §c§l➜ §fUtilize ").text("§b§n/checkpunish §b§n<jogador>").hover("§7Clique §7para §7copiar §7o §7comando").suggest("/checkpunish ").text("§f §fou §b§n/checkpunish §b§n<#ID>").hover("§7Clique §7para §7copiar §7o §7comando").suggest("/checkpunish #").text("§f.").send(p);
                     p.sendMessage("");
                 }
             }
